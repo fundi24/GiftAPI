93520403dad2fbd0731010bbdd34d7098bba668b
Fundi POSHI <fundi.poshi@condorcet.be>
2022-12-30 15:11:17 +0100

    Fin fonctionnalité activé/desactivé une liste

--- src/main/java/be/giftapi/api/ListGiftAPI.java
+++ src/main/java/be/giftapi/api/ListGiftAPI.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -18,6 +19,7 @@ import org.json.JSONObject;
 
 import be.giftapi.javabeans.Customer;
 import be.giftapi.javabeans.ListGift;
+import be.giftapi.javabeans.Notification;
 
 
 @Path("/listgift")
@@ -87,6 +89,24 @@ public class ListGiftAPI {
 		}
 		
 		return Response.status(Status.OK).entity(customers).build();
+	}
+	
+	@PUT
+	@Path("{id}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response updateNotification(@PathParam("id") int id, String data) {
+		JSONObject json = new JSONObject(data);
+		boolean status = json.getBoolean("status");
+		ListGift listGift = new ListGift();
+		listGift.setIdListGift(id);
+		listGift.setStatus(status);
+		boolean success = listGift.update();
+		if(!success) {
+			return Response.status(Status.SERVICE_UNAVAILABLE).build();
+		}
+		else {
+			return Response.status(Status.NO_CONTENT).build();
+		}
 	}
 	
 }
